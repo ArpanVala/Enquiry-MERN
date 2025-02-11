efe7bd8761475d3cf2cdd37c010509241763777c
Arpan Vala <valaarpan05@gmail.com>
2025-02-11 20:59:18 +0530

    frontend created

--- client/src/components/Enquiry.jsx
+++ client/src/components/Enquiry.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
+import EnquiryList from './EnquiryList';
 
 const Enquiry = () => {
+  const formSubmitted = (e) =>{
+    e.preventDefault();
+    let name = e.target.name.value;
+    let email = e.target.email.value;
+    let phone = e.target.phone.value;
+    let msg = e.target.msg.value;
+    alert(name + "\n" + email + "\n" + phone + "\n" + msg);
+  }
+
   return (
     <>
       <div className='container-fluid'>
-        <div className='container my-5'>
+        <div className='container my-5 py-3 px-5 rounded shadow-lg enquiry-div'>
+        <div className="row mb-4">
+          <div className="col text-center">
+            <h1>Enquiry portal</h1>
+          </div>
+        </div>
           <div className='row'>
             <div className='col-4'>
-              <div className='form-floating mb-3'>
+            <form onSubmit={formSubmitted}>
+                <div className='form-floating mb-3'>
                 <input
                   type='text'
                   name='name'
@@ -17,16 +33,37 @@ const Enquiry = () => {
                 />
                 <label for='floatingInput'>Name</label>
               </div>
-              <div className='form-floating'>
+              <div className='form-floating mb-3'>
                 <input
                   type='email'
                   name='email'
                   className='form-control'
-                  id='floatingPassword'
+                  id='floatingEmail'
                   placeholder='Enter your email'
                 />
-                <label for='floatingPassword'>Email</label>
+                <label for='floatingEmail'>Email</label>
               </div>
+               <div className='form-floating mb-3'>
+                <input
+                  type='number'
+                  name='phone'
+                  className='form-control'
+                  id='floatingPhone'
+                  maxLength={10}
+                  placeholder='Enter your Phone number'
+                />
+                <label for='floatingPhone'>Phone</label>
+              </div>
+              <div class="form-floating mb-3">
+                <textarea class="form-control" name="msg" placeholder="Leave a message here" id="floatingTextarea" ></textarea>
+                <label for="floatingTextarea">Message</label>
+              </div>
+               <button type="submit" class="btn btn-lg btn-dark w-100 " onSubmit={formSubmitted}>Submit</button>
+            </form>
+
+            </div>
+            <div className="col-8">
+             <EnquiryList/>
             </div>
           </div>
         </div>
---
+++ client/src/components/EnquiryList.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+
+const EnquiryList = () => {
+  return (
+    <table class='table table-striped'>
+      <thead class='table-dark'>
+        <tr>
+          <th scope='col'>sr.</th>
+          <th scope='col'>Name</th>
+          <th scope='col'>eMail</th>
+          <th scope='col'>Phone</th>
+          <th scope='col'>Message</th>
+          <th scope='col'>Action</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <th scope='row'>1</th>
+          <td>Mark</td>
+          <td>jdoi@jd.com</td>
+          <td>9082</td>
+          <td>Hello</td>
+          <td>
+            <button className='btn btn-sm btn-danger me-2'>delete</button>
+            <button className='btn btn-sm btn-primary'>edit</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  )
+}
+
+export default EnquiryList
