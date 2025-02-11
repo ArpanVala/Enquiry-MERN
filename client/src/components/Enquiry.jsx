07d137c11eb7e7d178fb069d520f9c7bedb59661
Arpan Vala <valaarpan05@gmail.com>
2025-02-12 00:30:30 +0530

    data inserted to DB from frontend with toastify

--- client/src/components/Enquiry.jsx
+++ client/src/components/Enquiry.jsx
@@ -1,26 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
+import {ToastContainer, toast} from 'react-toastify';
 import axios from 'axios';
 import EnquiryList from './EnquiryList';
 
 const Enquiry = () => {
+  const [formData, setFormData] = useState({
+    'name':'',
+    'email':'',
+    'phone':'',
+    'msg':''
+  })
+
+  const getvalue = (e) =>{
+    setFormData({...formData,[e.target.name]:e.target.value})
+    // or simply
+    // let inputName = e.target.name;
+    //let inputValue = e.target.value;
+    //let oldData = {...formData};
+    //oldData[inputName] = inputValue;
+    //setFormData(oldData);
+    
+  }
+
+
   const formSubmitted = (e) =>{
     e.preventDefault();
-    const formData ={
-      "name":e.target.name.value,
-      "email":e.target.email.value,
-      "phone":e.target.phone.value,
-      "msg":e.target.msg.value
-    }
+   
     console.log(formData);
     axios.post('http://localhost:8000/api/insert',formData).then((res)=>{
       console.log(res);
       alert("Hello "+ formData.name + " your enquiry has been submitted successfully"); 
+      toast.success('Enquiry submitted successfully');
     })
     
   }
 
   return (
     <>
+    <ToastContainer/>
       <div className='container-fluid'>
         <div className='container my-5 py-3 px-5 rounded shadow-lg enquiry-div'>
         <div className="row mb-4">
@@ -33,6 +50,8 @@ const Enquiry = () => {
             <form onSubmit={formSubmitted}>
                 <div className='form-floating mb-3'>
                 <input
+                onChange={getvalue}
+                value={formData.name}
                   type='text'
                   name='name'
                   className='form-control'
@@ -44,6 +63,8 @@ const Enquiry = () => {
               </div>
               <div className='form-floating mb-3'>
                 <input
+                 onChange={getvalue}
+                value={formData.email}
                   type='email'
                   name='email'
                   className='form-control'
@@ -55,6 +76,8 @@ const Enquiry = () => {
               </div>
                <div className='form-floating mb-3'>
                 <input
+                 onChange={getvalue}
+                value={formData.phone}
                   type='number'
                   name='phone'
                   className='form-control'
@@ -66,7 +89,7 @@ const Enquiry = () => {
                 <label htmlFor='floatingPhone'>Phone</label>
               </div>
               <div className="form-floating mb-3">
-                <textarea className="form-control" name="msg" placeholder="Leave a message here" id="floatingTextarea" required></textarea>
+                <textarea  onChange={getvalue} value={formData.msg} className="form-control" name="msg" placeholder="Leave a message here" id="floatingTextarea" required></textarea>
                 <label htmlFor="floatingTextarea">Message</label>
               </div>
                <button type="submit" className="btn btn-lg btn-dark w-100 " onSubmit={formSubmitted}>Submit</button>
